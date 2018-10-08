9ab26d1b756a661f09566e4c7ce73921511c04b1
Keith Simon <41575888+Krakaw@users.noreply.github.com>
2018-10-08 08:02:17 +0200

    Dockerfile builds during CMD (#254)
    To access the environment variables the dockerfile must build and serve during the CMD phase

--- Dockerfile
+++ Dockerfile
@@ -3,6 +3,6 @@ FROM node:9.11
 RUN mkdir /app
 WORKDIR /app
 ADD . /app/
-RUN npm install && npm run build
+RUN npm install
 
 CMD ["bash", "./scripts/start_env.sh"]
--- scripts/start_env.sh
+++ scripts/start_env.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
+
+if [ -z $ENV ]; then
+    ENV=$NODE_ENV
+fi
+
 if [ "$ENV" = "production" ]; then
+    npm run build
 	./node_modules/.bin/serve -s build
 else
+echo not
 	npm run start
 fi
