eb29acc15d8fc7020621924c2870c965683853b8
Keith Simon <41575888+Krakaw@users.noreply.github.com>
2018-10-11 09:16:48 +0200

    Build production files without serving them (#268)

--- scripts/start_env.sh
+++ scripts/start_env.sh
@@ -4,11 +4,17 @@ if [ -z $ENV ]; then
     ENV=$NODE_ENV
 fi
 
+if [ -z $NODE_ENV ]; then
+    NODE_ENV=$ENV
+fi
+
 if [ "$ENV" = "production" ]; then
     npm install
     npm run build
-	./node_modules/.bin/serve -s build
+    if [ ! -z "$PORT" ] && [ "$PORT" -ne 0 ]; then
+        echo "Serving static files from $PORT"
+	    ./node_modules/.bin/serve -s build
+	fi
 else
-echo not
 	npm run start
 fi
