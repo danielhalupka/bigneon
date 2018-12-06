e6de224ea40ec874f050f1feefc30784e41a3150
Stanimal <sdbondi@users.noreply.github.com>
2018-12-06 16:14:06 +0200

    Fix unsupported script syntax in buildspec

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 
+if [[ "$TARGET_ENV" != "staging" ]]; then
+  echo "The build will not auto-deploy"
+  exit 0
+fi
 BUCKET_NAME=$1
 echo "Deploying to $BUCKET_NAME"
 aws s3 sync --delete build s3://$BUCKET_NAME/build --cache-control max-age=3600
