2dae2cb5adab76416f6e5460ba9ed0c0e1ea2f03
Keith Simon <41575888+Krakaw@users.noreply.github.com>
2019-01-10 09:02:44 +0200

    Fix null error in nl2br (#903)

--- src/helpers/nl2br.js
+++ src/helpers/nl2br.js
@@ -1,6 +1,7 @@
 import React from "react";
 
 export default (text = "") => {
+	text = String(text || "");
 	return text.split("\n").map((item, key) => {
 		return <span key={key}>{item}<br/></span>;
 	});
