19fc06b32ed80e75fe48648cd653854be90e7e8a
Stan Bondi <sdbondi@users.noreply.github.com>
2019-02-01 07:38:21 +0200

    Invalidate cloudfront on deploy (#1036)

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -4,6 +4,11 @@ if [[ "$TARGET_ENV" != "staging" ]]; then
   echo "The build will not auto-deploy"
   exit 0
 fi
+
 BUCKET_NAME=$1
 echo "Deploying to $BUCKET_NAME"
 aws s3 sync --delete build s3://$BUCKET_NAME/build --cache-control max-age=3600
+
+if [[ ! -z "$CF_DISTRO_ID" ]]; then
+	aws cloudfront create-invalidation --distribution-id $CF_DISTRO_ID --paths /index.html /version.json
+fi
