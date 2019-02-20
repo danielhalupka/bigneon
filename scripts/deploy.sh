05096259e4d0c54463d17698c5d658eadbfd3e8f
Stan Bondi <sdbondi@users.noreply.github.com>
2019-02-20 09:43:33 +0200

    Dont auto deploy (#1126)

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,10 +1,5 @@
 #!/usr/bin/env bash
 
-if [[ "$TARGET_ENV" != "staging" ]]; then
-  echo "The build will not auto-deploy"
-  exit 0
-fi
-
 BUCKET_NAME=$1
 echo "Deploying to $BUCKET_NAME"
 aws s3 sync --delete build s3://$BUCKET_NAME/build --cache-control max-age=3600
