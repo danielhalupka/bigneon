717516115718f90399ca05d37fdb2f5b629a67c3
Jason van den Berg <jayvdb1@gmail.com>
2019-03-08 09:23:51 +0200

    Outputing stringyfied obj handling circular references (#1220)

--- src/stores/selectedEvent.js
+++ src/stores/selectedEvent.js
@@ -89,6 +89,28 @@ class SelectedEvent {
 				errorReporting.addBreadcrumb(`Bigneon().events.read typeof: ${typeof Bigneon().events.read}`);
 				errorReporting.addBreadcrumb(`Bigneon().events.read: ${Bigneon().events.read}`);
 
+				let cache = [];
+				const bnStringObj = JSON.stringify(Bigneon().events, function(key, value) {
+					if (typeof value === "object" && value !== null) {
+						if (cache.indexOf(value) !== -1) {
+							// Duplicate reference found
+							try {
+								// If this value does not reference a parent it can be deduped
+								return JSON.parse(JSON.stringify(value));
+							} catch (error) {
+								// discard key if value cannot be deduped
+								return;
+							}
+						}
+						// Store value in our collection
+						cache.push(value);
+					}
+					return value;
+				});
+				cache = null;
+
+				errorReporting.addBreadcrumb(bnStringObj);
+
 				errorReporting.captureMessage("Bigneon().events.read is not a function. Real value in bread crumb.");
 
 				Bigneon({}, {});
