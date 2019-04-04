64229d2b25f373b652b5b5de808a260813bd894e
Keith Simon <41575888+Krakaw@users.noreply.github.com>
2019-04-04 10:28:47 +0200

    Added ability to build specific branches within the docker file (#1313)

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,7 @@
 FROM mhart/alpine-node:10.13 as builder
 
+RUN apk update && \
+	apk add git
 #Create the app dir and install all of the dependencies
 WORKDIR /app
 COPY . ./
--- scripts/build_prod.sh
+++ scripts/build_prod.sh
@@ -1,5 +1,16 @@
 #!/bin/sh
 
+if [[ ! -z "$BUILD_BRANCH" ]]
+then
+	echo "Changing path to /"
+	cd /
+	echo "Deleting /app"
+	rm -rf /app
+	echo "Cloning $BUILD_BRANCH into /app"
+	git clone --single-branch --branch "$BUILD_BRANCH" https://github.com/big-neon/bn-web.git /app
+	echo "Changing path to /app"
+	cd /app
+fi
 npm install --production
-npm install --only=dev 
+npm install --only=dev
 npm run build
