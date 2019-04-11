a858a5e4999be14fd20b5430ee435152260255fa
Philip Robinson <simian@tari.com>
2019-04-11 14:34:05 +0200

    Fixed bug where a private ticket in a hold would crash the box office index page

--- src/components/pages/boxoffice/sales/Index.js
+++ src/components/pages/boxoffice/sales/Index.js
@@ -122,7 +122,7 @@ class TicketSales extends Component {
 		}
 
 		return ticketTypeIds.map(id => {
-			const { name, available, ticket_pricing, ...rest } = ticketTypes[id];
+			const { name, available, ticket_pricing, hidden,  ...rest } = ticketTypes[id];
 
 			let disabled = false;
 			let price_in_cents = 0;
@@ -130,6 +130,10 @@ class TicketSales extends Component {
 				price_in_cents = ticket_pricing.price_in_cents;
 			} else {
 				disabled = true; //No pricing yet so ticket probably not available for sale yet
+			}
+
+			if (hidden) {
+				return null;
 			}
 
 			return (
@@ -222,7 +226,7 @@ class TicketSales extends Component {
 
 		const { ticketTypes, holds } = boxOffice;
 
-		if (!ticketTypes || Object.keys(ticketTypes).length < 1) {
+		if ((!ticketTypes || Object.keys(ticketTypes).length < 1) && !holds) {
 			return null;
 		}
 
@@ -246,7 +250,9 @@ class TicketSales extends Component {
 				const { ticket_type_id, discount_in_cents, hold_type } = holds[id];
 
 				totalNumberSelected = totalNumberSelected + selectedHolds[id];
-
+				console.log("tt_id", ticket_type_id);
+				console.log("tt: ", ticketTypes);
+				console.log("tt[id]", ticketTypes[ticket_type_id]);
 				if (hold_type !== "Comp") {
 					const { ticket_pricing } = ticketTypes[ticket_type_id];
 					const { price_in_cents } = ticket_pricing;
