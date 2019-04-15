2ce21a96424468506f074be9a6943ad933b53c3d
Jason van den Berg <jayvdb1@gmail.com>
2019-04-15 14:12:48 +0200

    Guessing timezone instead of using localized_times (#1356)

--- src/stores/tickets.js
+++ src/stores/tickets.js
@@ -5,14 +5,6 @@ import notifications from "./notifications";
 import Bigneon from "../helpers/bigneon";
 import user from "./user";
 
-//TODO remove this when timezone is passed in API
-//https://github.com/big-neon/bn-api/issues/1091
-const momentFromLocalizedTime = (localized_time) => {
-	const n = localized_time.indexOf("-");
-	const dateString = localized_time.substring(0, n != -1 ? n : localized_time.length);
-	return moment(dateString, "ddd,  D MMM YYYY hh:mm:ss");
-};
-
 //TODO add filtering options to be used in display components
 
 class Tickets {
@@ -38,9 +30,14 @@ class Tickets {
 				data.forEach(ticketGroup => {
 					const event = ticketGroup[0];
 					const tickets = ticketGroup[1];
-					const { localized_times } = event;
 
-					event.eventDate = momentFromLocalizedTime(localized_times.event_start);
+					//TODO when the api returns the venue timezone, use that instead of the guess
+					//https://github.com/big-neon/bn-api/issues/1091
+					event.eventDate = moment(
+						event.event_start,
+						moment.HTML5_FMT.DATETIME_LOCAL_MS
+					).tz(moment.tz.guess());
+
 					event.formattedDate = event.eventDate.format("ddd MM/DD/YY, h:mm A");
 
 					ticketGroups.push({ event, tickets });
@@ -79,9 +76,13 @@ class Tickets {
 
 		let upcomingCount = 0;
 		this.groups.forEach(({ event }) => {
-			const { localized_times } = event;
-			const eventStartDate = momentFromLocalizedTime(localized_times.event_start);
-			const timeDifference = moment().diff(eventStartDate);
+			const { event_start } = event;
+			const eventStartDateUTC = (event.eventDate = moment.utc(
+				event.event_start,
+				moment.HTML5_FMT.DATETIME_LOCAL_MS
+			));
+
+			const timeDifference = moment.utc().diff(eventStartDateUTC);
 			if (timeDifference < 0) {
 				upcomingCount++;
 			}
